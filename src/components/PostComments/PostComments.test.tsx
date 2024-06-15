3e9dec02f483cf358aca8b2d5acc7a636883fc82
Isabela Ramos <isamrteixeira@gmail.com>
2024-06-15 15:21:38 -0300

    cobertura de teste - Post, PostComments e App

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -4,7 +4,40 @@ import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
+        //Renderiza o component PostComment
         render(<PostComment/>);
+        //Verifica se o botão 'Comentar' é mostrado após a renderização
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    //Teste - verifica a possibilidade de adicionar dois comentários
+    it('Deve adicionar dois comentários', () => {
+        //Renderiza o componente
+        render(<PostComment/>);
+
+        //Adiciona o primeiro comentário.
+            //Muda o valor da textarea
+        fireEvent.click(screen.getByTestId('comment-textarea'),{
+            target: {
+                value: 'Primeiro comentário adicionado via testes',
+            }
+        });
+        //Adiciona comentario clicando no botão
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        //Adiciona o segundo comentário.
+            //Muda novamente o valor da textarea
+        fireEvent.click(screen.getByTestId('comment-textarea'),{
+            target: {
+                value: 'Segundo comentário adicionado via testes',
+            }
+        });
+        //Adiciona comentario clicando no botão
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
+    });
+    
+
+
 });
